2b9717fde5ed59b14789d4d38a5e87ecd92317da
Lautaro Zuleta <lautarozule80@gmail.com>
2026-01-30 13:32:04 -0300

    feat: Add products module with controller, service, DTOs, TypeORM entity, and initial test files.

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', { unique: true })
+    name: string;
+
+    @Column('text')
+    description: string;
+
+    @Column('decimal', { default: 0 })
+    price: number;
+
+    @Column('int', { default: 0 })
+    stock: number;
+
+    @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+    created_at: Date;
+
+    @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+    updated_at: Date;
+}
